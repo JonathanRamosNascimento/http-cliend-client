3638bac91abc0ed5d687e6db9f484da284f42c1b
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-07-31 16:08:55 -0300

    error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Product } from './product.model';
 import { ProductsService } from './products.service';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +12,41 @@ import { Observable } from 'rxjs';
 export class AppComponent {
 
   simpleReqProductsObs$: Observable<Product[]>;
+  productsErrorHandling: Product[];
 
   constructor(
-    private productsService: ProductsService){}
+    private productsService: ProductsService,
+    private snackBar: MatSnackBar
+    ){}
 
   ngOnInit(){
   }
   
   getSimpleHttpRequest() {
     this.simpleReqProductsObs$ = this.productsService.getProducts();
+  }
+
+  getProductsWithErrorHandling() {
+    this.productsService.getProductsError().subscribe(
+      (prods) => {this.productsErrorHandling = prods; },
+      (err) => {
+        console.log(err);
+        console.log('Message: ' + err.error.msg);
+        console.log('Status code: ' + err.status);
+        let config = new MatSnackBarConfig();
+        config.duration = 2000;
+        config.panelClass = ['snack_err'];
+
+        if(err.status == 0 ) {
+          this.snackBar.open('Could not connect to the server', '', config);
+        }else{
+          this.snackBar.open(err.error.msg, '', config);
+        }
+      }
+    )
+  }
+
+  getProductsWithErrorHandlingOk() {
+
   }
 }
--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -14,4 +14,8 @@ export class ProductsService {
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.url}/products`);
   }
+
+  getProductsError(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.url}/productserr`);
+  }
 }
